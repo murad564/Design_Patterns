bedd19c0cf45cf67fadcc1c9682cea15136d0533
Mehe_od76@itstep.edu.az <Mehe_od76@itstep.edu.az>
2022-11-14 17:58:48 +0400

    Changes

---
+++ AbstractFactory/Program.cs
@@ -0,0 +1,176 @@
+﻿using static System.Console;
+
+
+
+
+abstract class AbstractCover { }
+
+class CocaColaCover : AbstractCover { }
+
+class FantaCover : AbstractCover { }
+
+class SpriteCover : AbstractCover { }
+
+
+
+
+
+abstract class AbstractWater { }
+
+class CocaColaWater : AbstractWater { }
+
+class FantaWater : AbstractWater { }
+
+class SpriteWater : AbstractWater { }
+
+
+
+
+
+
+
+abstract class AbstractBottle
+{
+    public abstract void Interact(AbstractWater water);
+    public abstract void Interact(AbstractCover water);
+}
+
+class CocaColaBottle : AbstractBottle
+{
+    public override void Interact(AbstractWater water)
+    {
+        WriteLine(this + " interacts with " + water);
+    }
+
+    public override void Interact(AbstractCover cover)
+    {
+        WriteLine(this + " interacts with " + cover);
+    }
+}
+
+class FantaBottle : AbstractBottle
+{
+    public override void Interact(AbstractWater water)
+    {
+        WriteLine(this + " interacts with " + water);
+    }
+
+    public override void Interact(AbstractCover cover)
+    {
+        WriteLine(this + " interacts with " + cover);
+    }
+}
+
+class SpriteBottle : AbstractBottle
+{
+    public override void Interact(AbstractWater water)
+    {
+        WriteLine(this + " interacts with " + water);
+    }
+
+    public override void Interact(AbstractCover cover)
+    {
+        WriteLine(this + " interacts with " + cover);
+    }
+}
+
+
+
+
+
+
+
+
+abstract class AbstractFactory
+{
+    public abstract AbstractBottle CreateBottle();
+    public abstract AbstractWater CreateWater();
+    public abstract AbstractCover CreateCover();
+}
+
+
+
+class CocaColaFactory : AbstractFactory
+{
+    public override AbstractBottle CreateBottle()
+        => new CocaColaBottle();
+
+    public override AbstractCover CreateCover()
+        => new CocaColaCover();
+
+    public override AbstractWater CreateWater()
+        => new CocaColaWater();
+}
+
+class FantaFactory : AbstractFactory
+{
+    public override AbstractBottle CreateBottle()
+        => new FantaBottle();
+
+    public override AbstractCover CreateCover()
+        => new FantaCover();
+
+    public override AbstractWater CreateWater()
+        => new FantaWater();
+}
+
+class SpriteFactory : AbstractFactory
+{
+    public override AbstractBottle CreateBottle()
+        => new SpriteBottle();
+
+    public override AbstractCover CreateCover()
+        => new SpriteCover();
+
+    public override AbstractWater CreateWater()
+        => new SpriteWater();
+}
+
+
+
+
+
+
+
+
+
+class Client
+{
+    private AbstractBottle _bottle;
+    private AbstractWater _water;
+    private AbstractCover _cover;
+
+    public Client(AbstractFactory factory)
+    {
+        _bottle = factory.CreateBottle();
+        _water = factory.CreateWater();
+        _cover = factory.CreateCover();
+    }
+
+    public void Run()
+    {
+        _bottle.Interact(_water);
+        _bottle.Interact(_cover);
+    }
+}
+
+
+
+class Program
+{
+    static void Main()
+    {
+        Client? client = null;
+
+        client = new Client(new CocaColaFactory());
+        client.Run();
+
+
+        client = new Client(new FantaFactory());
+        client.Run();
+
+
+        client = new Client(new SpriteFactory());
+        client.Run();
+    }
+}
---
+++ Prototype/Program.cs
@@ -0,0 +1,180 @@
+﻿using static System.Console;
+#nullable disable
+
+
+
+namespace Prototype_1
+{
+
+
+    public class Person
+    {
+        public int Age;
+        public DateTime BirthDate;
+        public string Name;
+        public IdInfo IdInfo;
+
+        public Person ShallowCopy()
+        {
+            return MemberwiseClone() as Person;
+        }
+
+        public Person DeepCopy()
+        {
+            Person clone = (Person)MemberwiseClone();
+            clone.IdInfo = new IdInfo(IdInfo.IdNumber);
+            return clone;
+        }
+    }
+
+
+
+    public class IdInfo
+    {
+        public int IdNumber;
+
+        public IdInfo(int idNumber)
+        {
+            IdNumber = idNumber;
+        }
+    }
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            p1.Age = 15;
+            p1.BirthDate = Convert.ToDateTime("2007-09-05");
+            p1.Name = "Emin Novruz";
+            p1.IdInfo = new IdInfo(777);
+
+            Person p2 = p1.ShallowCopy();
+            Person p3 = p1.DeepCopy();
+
+
+            WriteLine("Original values of p1, p2, p3:");
+
+            WriteLine("   \np1 instance values: ");
+            DisplayValues(p1);
+
+            WriteLine("   \np2 instance values:");
+            DisplayValues(p2);
+
+            WriteLine("   \np3 instance values:");
+            DisplayValues(p3);
+
+
+
+            p1.Age = 25;
+            p1.BirthDate = Convert.ToDateTime("1997-06-17");
+            p1.Name = "Royal";
+            p1.IdInfo.IdNumber = 135;
+
+            WriteLine("\nValues of p1, p2 and p3 after changes to p1:");
+
+            WriteLine("   \np1 instance values: ");
+            DisplayValues(p1);
+
+            WriteLine("   \np2 instance values (reference values have changed):");
+            DisplayValues(p2);
+
+            WriteLine("   \np3 instance values (everything was kept the same):");
+            DisplayValues(p3);
+
+        }
+
+
+        public static void DisplayValues(Person p)
+        {
+            WriteLine($"\tName: {p.Name}, Age: {p.Age}, BirthDate: {p.BirthDate.ToShortDateString()}");
+            WriteLine($"\tID#: {p.IdInfo.IdNumber}");
+        }
+    }
+}
+
+
+
+
+
+namespace Prototype_2
+{
+
+    //public interface IProtoType
+    //{
+    //    IProtoType Clone();
+    //}
+
+    public class Person : ICloneable
+    {
+        public int Age;
+        public DateTime BirthDate;
+        public string Name;
+        public IdInfo IdInfo;
+
+        //public IProtoType Clone()
+        //{
+        //    // Shallow Copy
+        //    return MemberwiseClone() as Person;
+        //}
+
+
+
+        object ICloneable.Clone()
+        {
+            return MemberwiseClone();
+        }
+    }
+
+
+    public class IdInfo
+    {
+        public int IdNumber;
+
+        public IdInfo(int idNumber)
+        {
+            IdNumber = idNumber;
+        }
+    }
+}
+
+
+
+
+
+namespace Prototype_3
+{
+    using System.Runtime.Serialization.Formatters.Binary;
+
+
+    [Serializable]
+    public abstract class IPrototype<T>
+    {
+        // Shallow copy
+        public T Clone()
+        {
+            return (T)MemberwiseClone();
+        }
+
+
+        // Deep Copy
+        public T DeepCopy()
+        {
+            using MemoryStream stream = new();
+            BinaryFormatter formatter = new();
+
+#pragma warning disable SYSLIB0011
+            formatter.Serialize(stream, this);
+
+            stream.Seek(0, SeekOrigin.Begin);
+
+            T copy = (T)formatter.Deserialize(stream);
+#pragma warning restore SYSLIB0011
+
+            return copy;
+        }
+    }
+}
